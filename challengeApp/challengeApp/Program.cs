3e9135f2f4a347c52f7d373d05e52a9997a4ef88
Dell <Dell@DESKTOP-5DG1PGE>
2023-10-19 17:25:03 +0200

    Zadanie "Dzień 4"

--- challengeApp/challengeApp/Program.cs
+++ challengeApp/challengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string gender = "Kobieta";
+string name = "Ewa";
+var age = 33;
+
+if (gender == "Kobieta" && age > 18)
+{
+   if (name == "Ewa" && age> 30)
+    {
+      Console.WriteLine( "Ewa lat 33");
+   }
+   else
+    {
+       Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+}
+else
+{
+    Console.WriteLine("Niepełnoletni Męzczyzna");
+}
