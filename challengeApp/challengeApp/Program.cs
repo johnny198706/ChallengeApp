59a87a8541d73568070c1976c363d32f07391aeb
Dell <Dell@DESKTOP-5DG1PGE>
2023-10-25 20:41:31 +0200

    Dzień 6

--- challengeApp/challengeApp/Program.cs
+++ challengeApp/challengeApp/Program.cs
@@ -1,105 +1,51 @@
-﻿
+﻿using challengeApp;
 
-using System.Diagnostics.CodeAnalysis;
+Employee employee1 = new Employee("Rafał", "Stefański", 32 ); 
+Employee employee2 = new Employee("Mateusz", "Zieliński", 27 );
+Employee employee3 = new Employee("Natalia", "Mucha", 37);
 
-int number = 435007657;
-string numberInStrin = number.ToString();
-char[] letters = number.ToString().ToArray();
-
-
-
-
-string[] numbers = new string[10];
-numbers[0] = ("0=>");
-numbers[1] = ("1=>");
-numbers[2] = ("2=>");
-numbers[3] = ("3=>");
-numbers[4] = ("4=>");
-numbers[5] = ("5=>");
-numbers[6] = ("6=>");
-numbers[7] = ("7=>");
-numbers[8] = ("8=>");
-numbers[9] = ("9=>");
+employee1.AddScore(8);
+employee1.AddScore(9);
+employee1.AddScore(7);
+employee1.AddScore(9);
+employee1.AddScore(10);
 
 
+employee2.AddScore(7);
+employee2.AddScore(6);
+employee2.AddScore(4);
+employee2.AddScore(3);
+employee2.AddScore(6);
 
-int counter = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+employee3.AddScore(3);
+employee3.AddScore(5);
+employee3.AddScore(7);
+employee3.AddScore(8);
+employee3.AddScore(7);
+
+ var result = employee1.Result;
+var result1 = employee2.Result;
+var result2 = employee3.Result;
 
 
-
-
-foreach (char letter in letters)
+List<Employee> employees = new List<Employee>() 
 {
-    if(letter == '0')
+employee1, employee2, employee3
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach(var employee in employees)
+{
+    if(employee.Result > maxResult)
     {
-        counter++;
+        employeeWithMaxResult = employee;
+
+        Console.WriteLine("Pracownik otrzymał najwiecej punktów:" + result + " Imie: " + employeeWithMaxResult.Name + " Nazwisko: " + employeeWithMaxResult.Surname + " Wiek: " + employeeWithMaxResult.Age);
+        Console.ReadLine();
 
     }
-    else if(letter == '1')
-    {
-        counter1++;
     
-    }
-    else if (letter == '2')
-    {
-        counter2++;
 
-    }
-    else if(letter == '3') 
-    {
-        counter3++;
-    
-    }
-    else if(letter == '4')
-    {
-        counter4++;
-   
-    }
-    else if(letter == '5')
-    {
-        counter5++;
- 
-    }
-    else if(letter == '6')
-    {
-        counter6++;
-
-    }
-    else if(letter == '7')
-    {
-        counter7++;
-
-    }
-    else if(letter == '8')
-    {
-        counter8++;
-
-    }
-    else if(letter == '9')
-    {
-        counter9++;
-  
-    }
 }
-
-
-Console.WriteLine(numbers[0] + counter);
-Console.WriteLine(numbers[1] + counter1);
-Console.WriteLine(numbers[2] + counter2);
-Console.WriteLine(numbers[3] + counter3);
-Console.WriteLine(numbers[4] + counter4);
-Console.WriteLine(numbers[5] + counter5);
-Console.WriteLine(numbers[6] + counter6);
-Console.WriteLine(numbers[7] + counter7);
-Console.WriteLine(numbers[8] + counter8);
-Console.WriteLine(numbers[9] + counter9);
-
